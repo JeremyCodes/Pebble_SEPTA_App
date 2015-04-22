b230f4740efc7ec885b9560082df1351b69b4fab
JeremyCodes <jeremy.henderson@gmail.com>
2015-04-22 15:07:31 -0400

    Look up data is working
    Looking up based on location is working. Need to extract data from resulting JSON.

--- src/main.c
+++ src/main.c
@@ -1,191 +1,34 @@
+/*
+ * main.c
+ * Sets up a Window and a TextLayer.
+ */
+
 #include <pebble.h>
-#include <string.h>
-
-#define WEATHER_LAYER_BUFFER_SIZE 32
-
-#define DEBUG S_TRUE
-
-#define WEATHER_UPDATE_INTERVAL  30
-#define SEPTA_UPDATE_INTERVAL    10
-  
-enum {
-  KEY_TEMPERATURE = 2,
-  KEY_CONDITIONS = 1
-};
 
 static Window *s_main_window;
-static Layer *s_canvas_layer;
-static TextLayer *s_time_layer;
-static TextLayer *s_date_layer;
-static TextLayer *s_weather_layer;
-static TextLayer *s_septa_layer1;
-static TextLayer *s_septa_layer2;
+static TextLayer *s_text_layer;
 
-static int s_weather_updating;
+enum {
+  KEY_STATION = 0,
+  KEY_TIME = 1,
+  KEY_TARDINESS = 2
+};
 
-static void update_time()
-{
-	// Get a tm structure
-	time_t temp = time(NULL); 
-	struct tm *tick_time = localtime(&temp);
-
-  // Create two long-lived buffer and a pointer to reference;
-  static char date_buffer_1_digit[] = "0";
-  static char date_buffer_2_digit[] = "00";
-  static char *date_buffer[2];
-  static char long_buffer[] = "00:00";
-  static char short_buffer[] = "0:00";
-  static char *buffer[6]; //[5];
-  *buffer = long_buffer;
-  *date_buffer = date_buffer_2_digit;
-
-	// Write the current hours and minutes into the buffer
-	if(clock_is_24h_style() == true) 
-	{
-	  // Use 24 hour format
-		strftime(long_buffer, sizeof("00:00"), "%H:%M", tick_time);
-		*buffer = long_buffer;
-	} 
-	else // Use 12 hour format
-	{
-		strftime(long_buffer, sizeof("00:00"), "%I:%M", tick_time);
-		
-		*buffer = long_buffer;
-		
-		if (long_buffer[0] == '0')
-		{
-			short_buffer[0] = long_buffer[1];
-			short_buffer[2] = long_buffer[3];
-			short_buffer[3] = long_buffer[4];
-			*buffer = short_buffer;
-		}
-	}	 
-	// Display this time on the TextLayer
-	text_layer_set_text(s_time_layer, *buffer);
-  
-  strftime(date_buffer_2_digit, sizeof(date_buffer_2_digit), "%d", tick_time);
-  if (date_buffer_2_digit[0] == '0')
-  {
-    date_buffer_1_digit[0] = date_buffer_2_digit[1];
-    *date_buffer = date_buffer_1_digit;
-  }
-  text_layer_set_text(s_date_layer, *date_buffer);
-}
-
-static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
-  GRect bounds = layer_get_bounds(this_layer);
-
-  // get a background
-  graphics_context_set_fill_color(ctx, GColorBlack);
-  graphics_fill_rect(ctx, GRect(0, 0, 144, 168), 0, GCornerNone);
-  
-  // Draw Bisecting line
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, GRect(0, ((bounds.size.h/2) -1), 144, 2), 0, GCornerNone);  
-  
-  // Draw the 'border'
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, GRect(8, 53, 128, 59), 0, GCornerNone);
-}
-
-static void main_window_load(Window *window) 
-{
+static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect window_bounds = layer_get_bounds(window_layer);
 
-  // Create Layer
-  s_canvas_layer = layer_create(GRect(0, 0, window_bounds.size.w, window_bounds.size.h));
-  layer_add_child(window_layer, s_canvas_layer);
-
-  // Set the update_proc
-  layer_set_update_proc(s_canvas_layer, canvas_update_proc);
-  
-  // Add Time layer
-	s_time_layer = text_layer_create(GRect(10, 55, 124, 55));
-	text_layer_set_background_color(s_time_layer, GColorBlack);
-	text_layer_set_text_color(s_time_layer, GColorClear);
-	
-	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-	
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
-
-  // Add Date Layer
-	s_date_layer = text_layer_create(GRect(119, 95, 15, 15));
-	text_layer_set_background_color(s_date_layer, GColorBlack);
-	text_layer_set_text_color(s_date_layer, GColorClear);
-	
-	text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
-	text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
-	
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
-  
-	// Add weather text layer
-	s_weather_layer = text_layer_create(GRect(0, 12, 144, 30));
-	text_layer_set_background_color(s_weather_layer, GColorClear);
-	text_layer_set_text_color(s_weather_layer, GColorWhite);
-	text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
-	text_layer_set_text(s_weather_layer, "Updating...");
-	
-	text_layer_set_font(s_weather_layer,  fonts_get_system_font(FONT_KEY_GOTHIC_24));
-	
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
-  
-	// Add SEPTA text layers
-	s_septa_layer1 = text_layer_create(GRect(0, 118, 144, 30));
-	text_layer_set_background_color(s_septa_layer1, GColorClear);
-	text_layer_set_text_color(s_septa_layer1, GColorWhite);
-	text_layer_set_text_alignment(s_septa_layer1, GTextAlignmentLeft);
-	text_layer_set_text(s_septa_layer1, " JEF> 00:00a On Time");
-	
-	text_layer_set_font(s_septa_layer1, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-	
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_septa_layer1));
-  
-  s_septa_layer2 = text_layer_create(GRect(0, 135, 144, 30));
-	text_layer_set_background_color(s_septa_layer2, GColorClear);
-	text_layer_set_text_color(s_septa_layer2, GColorWhite);
-	text_layer_set_text_alignment(s_septa_layer2, GTextAlignmentLeft);
-	text_layer_set_text(s_septa_layer2, " JEF>   6:00a On Time");
-	
-	text_layer_set_font(s_septa_layer2, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-	
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_septa_layer2));
+  // Create output TextLayer
+  s_text_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 5, window_bounds.size.h));
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text(s_text_layer, "Updating Train Info...");
+  text_layer_set_overflow_mode(s_text_layer, GTextOverflowModeWordWrap);
+  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 }
 
-static void main_window_unload(Window *window) 
-{
-	text_layer_destroy(s_septa_layer1);
-  text_layer_destroy(s_septa_layer2);
-	text_layer_destroy(s_weather_layer);
-	text_layer_destroy(s_time_layer);
-}
-
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
-{
-	update_time();
-//  char new_text_buffer[32];
-  
-  // Get weather update every 30 minutes
-  if ( ((tick_time->tm_min % WEATHER_UPDATE_INTERVAL) == 0) || 
-       (s_weather_updating) )
-  {
-    APP_LOG(APP_LOG_LEVEL_INFO, "Weather updating...");
-    // Mark for update
-    //snprintf(new_text_buffer, (3 + sizeof(text_layer_get_text(s_weather_layer))), "%s(u)", text_layer_get_text(s_weather_layer)) ;
-//    text_layer_set_text(s_weather_layer, "Updating...");
-    
-    // Begin dictionary
-    DictionaryIterator *iter;
-    app_message_outbox_begin(&iter);
-  
-    // Add a key-value pair
-    dict_write_uint8(iter, 0, 0);
-  
-    // Send the message!
-    s_weather_updating = S_TRUE;
-    app_message_outbox_send();
-  }
+static void main_window_unload(Window *window) {
+  // Destroy output TextLayer
+  text_layer_destroy(s_text_layer);
 }
 
 
@@ -194,9 +37,10 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   APP_LOG(APP_LOG_LEVEL_INFO, "Received Inbox message!");
   
   // Store incoming information
-  static char temperature_buffer[8];
-  static char conditions_buffer[32];
-  static char weather_layer_buffer[WEATHER_LAYER_BUFFER_SIZE];
+  static char station_buffer[32];
+  static char time_buffer[32];
+  static char tardiness_buffer[32];
+  static char merged_buffer[256];
   
   // Read first item
   Tuple *t = dict_read_first(iterator);
@@ -205,23 +49,27 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   while(t != NULL) {
     // Which key was received?
     switch(t->key) {
-      case KEY_TEMPERATURE:
-        snprintf(temperature_buffer, sizeof(temperature_buffer), "%dF", (int)t->value->int32);
-        s_weather_updating = S_FALSE;
+      case KEY_STATION:
+        snprintf(station_buffer, sizeof(station_buffer), "%s", t->value->cstring);
+//        s_weather_updating = S_FALSE;
         break;
-      case KEY_CONDITIONS:
-        snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
-        s_weather_updating = S_FALSE;
+      case KEY_TIME:
+        snprintf(time_buffer, sizeof(time_buffer), "%s", t->value->cstring);
+//        s_weather_updating = S_FALSE;
         break;
+      case KEY_TARDINESS:
+        snprintf(tardiness_buffer, sizeof(tardiness_buffer), "%s", t->value->cstring);
+//        s_weather_updating = S_FALSE;
+        break;      
       default:
         APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
         break;
     }
     
     // Assemble full string and display
-    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
-    text_layer_set_text(s_weather_layer, weather_layer_buffer);
-    
+    snprintf(merged_buffer, sizeof(merged_buffer), "%s\n%s    %s", station_buffer, time_buffer, tardiness_buffer);
+    text_layer_set_text(s_text_layer, merged_buffer);
+   
     // Look for next item
     t = dict_read_next(iterator);
   }
@@ -241,39 +89,31 @@ static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
 }
 
 
-static void init() 
-{
-  s_weather_updating = S_TRUE;
-	s_main_window = window_create();
-	
-	window_set_window_handlers(s_main_window, 
-												(WindowHandlers) 
-												{
-													.load  = main_window_load,
-													.unload = main_window_unload
-												});
-	window_stack_push(s_main_window, true);
-	update_time();
-	tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-	
-	// Register callbacks
+static void init() {
+  // Create main Window
+  s_main_window = window_create();
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+  window_stack_push(s_main_window, true);
+  
+  // Register callbacks
 	app_message_register_inbox_received(inbox_received_callback);
 	app_message_register_inbox_dropped(inbox_dropped_callback);
 	app_message_register_outbox_failed(outbox_failed_callback);
 	app_message_register_outbox_sent(outbox_sent_callback);
-	
-	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
-
+  
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 }
 
-static void deinit() 
-{
-	window_destroy(s_main_window);
+static void deinit() {
+  // Destroy main Window
+  window_destroy(s_main_window);
 }
 
-int main(void) 
-{
-	init();
-	app_event_loop();
-	deinit();
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
 }
--- src/pebble-js-app.js
+++ src/pebble-js-app.js
@@ -1,3 +1,11 @@
+var RADIUS = 1.0; //Miles
+var DEBUG_ON_PC = false;
+
+var Stations = {"stations" : [
+  {"name": "Secane", "lookup_name": "Secane", "lat": "39.91574", "long": "-75.30986"},
+  {"name": "Suburban", "lookup_name": "Suburban%20Station","lat": "39.95425", "long": "-75.167"},
+]};
+
 var xhrRequest = function (url, type, callback) {
   var xhr = new XMLHttpRequest();
   xhr.onload = function () {
@@ -7,55 +15,128 @@ var xhrRequest = function (url, type, callback) {
   xhr.send();
 };
 
-function locationSuccess(pos) {
-  // Construct URL
-  var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
-      pos.coords.latitude + '&lon=' + pos.coords.longitude;
+  //http://www3.septa.org/hackathon/Arrivals/Suburban%20Station/5/
 
-  // Send request to OpenWeatherMap
-  xhrRequest(url, 'GET', 
-    function(responseText) {
-      // responseText contains a JSON object with weather info
-      try {
-        var json = JSON.parse(responseText);
+function locationSuccess(pos) {
+  console.log("user location retrieved. (" + pos.coords.latitude + ", " + pos.coords.longitude + ")");
   
-        // Temperature in Kelvin requires adjustment
-        var temperature = (((Math.round(json.main.temp - 273.15)) * 1.8) + 32);
-        console.log('json.main.temp is ' + json.main.temp);
-        console.log('Temperature is ' + temperature);
+  //for testing
+  if (DEBUG_ON_PC)
+  {
+    pos.coords.latitude = 39.9529899;
+    pos.coords.longitude = -75.1684441;
+  }
   
-        // Conditions
-        var conditions = json.weather[0].main;      
-        console.log('Conditions are ' + conditions);
-        
-        // Assemble dictionary using our keys
-        var dictionary = {
-          'KEY_TEMPERATURE': temperature,
-          'KEY_CONDITIONS': conditions
-        };
-        
-        // Send to Pebble
-        Pebble.sendAppMessage(dictionary,
-          function(e) {
-            console.log('Weather info sent to Pebble successfully!');
-          },
-          function(e) {
-            console.log('Error sending weather info to Pebble!');
-          }
-        );
+  // determine which station you are currently located near.
+  var station = getNearestStation(pos);
+  
+  if (station !== null)
+  { 
+    // Construct URL  
+  var url = 'http://www3.septa.org/hackathon/Arrivals/' + station.lookup_name + '/1/';
+  //  var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
+  //      pos.coords.latitude + '&lon=' + pos.coords.longitude;
+  
+    // Send request to OpenWeatherMap
+    xhrRequest(url, 'GET', 
+      function(responseText) {
+        // responseText contains a JSON object with train info
+        try {
+          
+          var json = JSON.parse(responseText);
+          
+          /*
+          // Temperature in Kelvin requires adjustment
+          var temperature = (((Math.round(json.main.temp - 273.15)) * 1.8) + 32);
+          console.log('json.main.temp is ' + json.main.temp);
+          console.log('Temperature is ' + temperature);
+    
+          // Conditions
+          var conditions = json.weather[0].main;      
+          console.log('Conditions are ' + conditions);
+          */
+          // Assemble dictionary using our keys
+          var dictionary = {
+            'KEY_STATION': station.name,
+            'KEY_TIME': "4:34p",
+            'KEY_TARDINESS': "6 min"
+          };
+          
+          // Send to Pebble
+          Pebble.sendAppMessage(dictionary,
+            function(e) {
+              console.log('Train info sent to Pebble successfully!');
+            },
+            function(e) {
+              console.log('Error sending train info to Pebble!');
+            }
+          );
+        }
+        catch (e) {
+          console.log("Exception caught in xhrRequest: "+ e);
+        }
       }
-      catch (e) {
-        console.log("Exception caught in xhrRequest: "+ e);
-      }
+    );
+  } 
+  else // Out of septa station range send that note back.
+  {
+          var dictionary = {
+            'KEY_STATION': "Out of Range",
+            'KEY_TIME': "",
+            'KEY_TARDINESS': ""
+          };
+            // Send to Pebble
+          Pebble.sendAppMessage(dictionary,
+            function(e) {
+              console.log('Train info sent to Pebble successfully!');
+            },
+            function(e) {
+              console.log('Error sending train info to Pebble!');
+            }
+          );
+  }
+}
+
+function getNearestStation(pos) {
+  var return_value = null;
+  
+  for (var i = 0; i < Stations.stations.length; i++)
+  {
+    var distance = mathGeoDistance( Stations.stations[i].lat, Stations.stations[i].long, pos.coords.latitude, pos.coords.longitude, true );
+      
+    if (distance <= RADIUS)
+    {
+      console.log("nearest station found: " + Stations.stations[i].name);
+      return Stations.stations[i];
     }
-  );
+  }
+  console.log("No local station found!");
+  return return_value;
+}
+
+function mathGeoDistance( p_lat1, p_lng1, p_lat2, p_lng2, p_miles ) {
+    var M_PI = 3.14;
+    var pi80 = M_PI / 180;
+    var lat1 = p_lat1 * pi80;
+    var lng1 = p_lng1 * pi80;
+    var lat2 = p_lat2 * pi80;
+    var lng2 = p_lng2 * pi80;
+
+    var r = 6372.797; // mean radius of Earth in km
+    var dlat = lat2 - lat1;
+    var dlng = lng2 - lng1;
+    var a = Math.sin(dlat / 2) * Math.sin(dlat / 2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dlng / 2) * Math.sin(dlng / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var km = r * c;
+
+    return (p_miles ? (km * 0.621371192) : km);
 }
 
 function locationError(err) {
   console.log('Error requesting location!');
 }
 
-function getWeather() {
+function getTrain() {
   navigator.geolocation.getCurrentPosition(
     locationSuccess,
     locationError,
@@ -63,22 +144,13 @@ function getWeather() {
   );
 }
 
-function getSepta(){
-  
-}
-
-function parseEvent() {
-  getWeather();
-  getSepta();
-}
-
 // Listen for when the watchface is opened
 Pebble.addEventListener('ready', 
   function(e) {
     console.log('PebbleKit JS ready!');
     
-    // Get the initial weather
-    getWeather();
+    // Get the initial train
+    getTrain();
   }
 );
 
@@ -86,6 +158,6 @@ Pebble.addEventListener('ready',
 Pebble.addEventListener('appmessage',
   function(e) {
     console.log('AppMessage received!');
-    parseEvent();
+    getTrain();
   }                     
 );
